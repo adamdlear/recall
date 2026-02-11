7981953995c3df92d23f866d98c4e3614997159a
Adam Lear <adamlear12@gmail.com>
2026-02-10 20:52:08 -0600

    feat: setup Elysia fullstack server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+import { staticPlugin } from '@elysiajs/static'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    await staticPlugin({
+      prefix: '/'
+    })
+  )
+  .get('/api/hello', () => ({
+    message: 'Hello from Elysia 🚀'
+  }))
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
