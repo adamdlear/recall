22333296d976a13aa0cef839786cc8ed140921e8
Adam Lear <adamlear12@gmail.com>
2026-02-12 21:35:30 -0600

    feat: add shadcn to project

--- public/login/login.tsx
+++ public/login/login.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from "react-dom/client";
 
 import "@public/global.css";
+import { Button } from "@components/ui/button";
 
 function App() {
 	return (
 		<main>
 			<p>My Login Page</p>
+			<a>
+				<Button>Login with Provider</Button>
+			</a>
 		</main>
 	);
 }
