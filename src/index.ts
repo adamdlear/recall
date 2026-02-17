15b497d870da3970b0b6a7ef0de4d42b1d8afe62
Adam Lear <adamlear12@gmail.com>
2026-02-17 01:14:54 -0600

    docs: add openapi docs to api with support for better-auth

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
-import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors';
+import { openapi } from '@elysiajs/openapi';
 import { staticPlugin } from "@elysiajs/static";
+import { Elysia } from "elysia";
+import { auth, OpenAPI } from './lib/auth';
 
 const app = new Elysia()
+	.use(
+		openapi({
+			documentation: {
+				components: await OpenAPI.components,
+				paths: await OpenAPI.getPaths()
+			}
+		})
+	)
+	.use(
+		cors({
+			origin: true,
+			credentials: true
+		})
+	)
 	.use(
 		await staticPlugin({
 			prefix: "/",
 		}),
 	)
+	.mount(auth.handler)
 	.get("/api/hello", () => ({
 		message: "Hello from Elysia 🚀",
 	}))
 	.listen(3000);
 
 console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	`🦊 Elysia is running at ${app.server?.protocol}://${app.server?.hostname}:${app.server?.port}`,
 );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,46 @@
-import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { openAPI } from 'better-auth/plugins';
+import { db } from "../db";
+import * as schema from "../db/schema/auth";
 
 export const auth = betterAuth({
+	basePath: "/api",
 	database: drizzleAdapter(db, {
 		provider: "pg",
+		schema,
 	}),
+	plugins: [
+		openAPI()
+	],
+	socialProviders: {
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+		}
+	}
 });
+
+let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>
+const getSchema = async () => (_schema ??= auth.api.generateOpenAPISchema())
+
+export const OpenAPI = {
+	getPaths: (prefix = '/auth/api') =>
+		getSchema().then(({ paths }) => {
+			const reference: typeof paths = Object.create(null)
+
+			for (const path of Object.keys(paths)) {
+				const key = prefix + path
+				reference[key] = paths[path]
+
+				for (const method of Object.keys(paths[path])) {
+					const operation = (reference[key] as any)[method]
+
+					operation.tags = ['Better Auth']
+				}
+			}
+
+			return reference
+		}) as Promise<any>,
+	components: getSchema().then(({ components }) => components) as Promise<any>
+} as const
